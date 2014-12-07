c8465c8ee3eecf484f58067501876d01a81a8aff
Ben Green <s.edwin.green@gmail.com>
2014-12-07 18:43:24 -0500

    http shit

--- server.c
+++ server.c
@@ -14,15 +14,21 @@ void DieWithError(char *errorMessage);  /* External error handling function */
 
 int main(int argc, char *argv[])
 {
-    int sock;                        	/* Socket */
-    struct sockaddr_in servAddr;     	/* Local address */
-    struct sockaddr_in clntAddr;     	/* Client address */
-    unsigned short servPort;         	/* Server port */
-    unsigned int cliAddrLen;		/* Length of message */
-    char msgBuffer[STRMAX];		/* Buffer fo the incoming message */
-    int msgSize;                     	/* Size of received message */
-    
+    int sock;                    /* Socket */
+    struct sockaddr_in servAddr; /* Local address */
+    struct sockaddr_in clntAddr; /* Client address */
+    unsigned short servPort;     /* Server port */
+    unsigned int cliAddrLen;     /* Length of message */
+    char msgBuffer[STRMAX];	     /* Buffer for the incoming message */
+    int msgSize;                 /* Size of received message */
 
+	int hSock;                     /* socket for http requests*/
+	struct sockaddr_in hServAddr;    /* address of http server */
+	unsigned short gpsPort = 8082;
+	unsigned short dGpsPort = 8084;
+	unsigned short lasersPort = 8083;
+	char httpBuffer[STRMAX];       /* Buffer for http request */
+    
     if (argc != 2)         	/* Test for correct number of parameters */
     {
         fprintf(stderr,
@@ -38,14 +44,25 @@ int main(int argc, char *argv[])
         DieWithError("socket() failed");
 
     /* Construct local address structure */
-    memset(&servAddr, 0, sizeof(servAddr));   /* Zero out structure */
+    memset(&servAddr, 0, sizeof(servAddr));       /* Zero out structure */
     servAddr.sin_family = AF_INET;                /* Internet address family */
     servAddr.sin_addr.s_addr = htonl(INADDR_ANY); /* Any incoming interface */
-    servAddr.sin_port = htons(servPort);      /* Local port */
+    servAddr.sin_port = htons(servPort);          /* Local port */
 
     /* Bind to the local address */
     if (bind(sock, (struct sockaddr *) &servAddr, sizeof(servAddr)) < 0)
-        DieWithError("bind() failed");
+        DieWithError("bind() failed");	
+		
+	/* Http setup */
+	if ( (hSock = socket(AF_INET, SOCK_STREAM, 0)) < 0 )
+		DieWithError("Socket");
+		
+    memset(&hServAddr, 0, sizeof(hServAddr));      /* Zero out structure */
+    hServAddr.sin_family = AF_INET;                /* Internet address family */
+    hServAddr.sin_addr.s_addr = htonl(INADDR_ANY); /* Any incoming interface */
+//    hServAddr.sin_port = htons(gpsPort);         /* Local port */
+	if (inet_addr("130.127.192.62", &hServAddr.sin_addr.s_addr) == 0)
+		DieWithError("Socket Failed");
 
     for (;;) /* Run forever */
     {
@@ -57,7 +74,24 @@ int main(int argc, char *argv[])
             (struct sockaddr *) &clntAddr, &cliAddrLen)) < 0)
             DieWithError("recvfrom() failed");
 	
-
+		/* stupid http stuff, ignore mostly cause stupid */
+		/* determine and set variables */
+		if (msgType == GPS || msgType == MOVE || msgType == TURN || msgType == STOP) {
+			hServAddr.sin_port = htons(gpsPort);
+		} else if (msgType == DGPS) {
+			hServAddr.sin_port = htons(dGpsPort);
+		} else if (msgType == LASERS) {
+			hServAddr.sin_port = htons(lasersPort);
+		}
+		if (connect(hSock, (struct sockaddr *) &hServAddr, sizeof(hServAddr)) != 0)
+			DieWithError("bind() failed");
+		if (msgType == GPS || msgType == DGPS || msgType == LASERS) {
+			sprintf(httpBuffer, "Get %s HTTP/1.0\n\n", msgType);
+		} else if (msgType == MOVE)
+			sprintf(httpBuffer, "Get %s HTTP/1.0\n\n", argv[2]);
+		}
+		/* send request */
+		send(hServAddr, httpBuffer, strlen(httpBuffer), 0);
     }
     /* NOT REACHED */
 }
